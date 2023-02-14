aa3a21e4ff4deb17c97e39e5c3594e003a71f1fa
Juliana <jumiranda0501@gmail.com>
2023-02-14 18:10:34 -0300

    Add images

--- src/contact.js
+++ src/contact.js
@@ -9,13 +9,55 @@ const contact = () => {
     container.classList.add("contact-container");
 
     container.appendChild(description);
-    addSource(container, "background", "author", "#", "#");
-    addSource(container, "img1", "author", "#", "#");
-    addSource(container, "img2", "author", "#", "#");
-    addSource(container, "img3", "author", "#", "#");
-    addSource(container, "img4", "author", "#", "#");
-    addSource(container, "img5", "author", "#", "#");
-    addSource(container, "img6", "author", "#", "#");
+    addSource(
+        container, 
+        "Background", 
+        "Mihai Moisa", 
+        "https://unsplash.com/pt-br/fotografias/Djtc1T38-GY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", 
+        "https://unsplash.com/@moisamihai092?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
+    addSource(
+        container, 
+        "Coffee", 
+        "Jeremy Yap", 
+        "https://unsplash.com/pt-br/fotografias/jn-HaGWe4yw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", 
+        "https://unsplash.com/es/@jeremyyappy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
+    addSource(
+        container, 
+        "Croissant", 
+        "Kobby Mendez", 
+        "https://unsplash.com/pt-br/fotografias/iyM-XTsTiek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", 
+        "https://unsplash.com/@kobbymendez?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
+    addSource(
+        container, 
+        "Pancake", 
+        "Joshua Ryder", 
+        "https://unsplash.com/pt-br/fotografias/y4K7Ydp2z3Y?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", 
+        "https://unsplash.com/@photos_by_ryder?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
+    addSource(
+        container, 
+        "Sandwich",
+        "Eiliv Aceron",
+        "https://unsplash.com/pt-br/fotografias/mAQZ3X_8_l0?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+        "https://unsplash.com/pt-br/@shootdelicious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
+    addSource(
+        container, 
+        "Bread with egg", 
+        "Ben Kolde", 
+        "https://unsplash.com/pt-br/fotografias/FFqNATH27EM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", 
+        "https://unsplash.com/ja/@benkolde?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
+    addSource(
+        container, 
+        "Milkshake", 
+        "Jonathan Borba", 
+        "https://unsplash.com/pt-br/fotografias/7TeR1A1MUpM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", 
+        "https://unsplash.com/@jonathanborba?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    );
 
     return container;
     
--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,20 @@
+import coffee from "./img/coffee.jpg";
+import croissant from "./img/croissant.jpg";
+import pancake from "./img/pancake.jpg";
+import sandwich from "./img/sandwich.jpg";
+import egg from "./img/egg.jpg";
+import milkshake from "./img/milkshake.jpg";
+
 const menu = () => {
 
     // elements
     const menu_list = document.createElement("ul");
-    addItem("Item 1", menu_list);
-    addItem("Item 2", menu_list);
-    addItem("Item 3", menu_list);
-    addItem("Item 4", menu_list);
-    addItem("Item 5", menu_list);
-    addItem("Item 6", menu_list);
+    addItem(coffee, "Coffee", menu_list);
+    addItem(croissant, "Croissant", menu_list);
+    addItem(pancake, "Pancake", menu_list);
+    addItem(sandwich, "Sandwich", menu_list);
+    addItem(egg, "Bread with egg", menu_list);
+    addItem(milkshake, "Milkshake", menu_list);
 
     menu_list.setAttribute("id", "tab-content");
     menu_list.classList.add("menu");
@@ -16,9 +23,14 @@ const menu = () => {
 
 }
 
-function addItem(name, container) {
+function addItem(source, alt, container) {
     const item = document.createElement("li");
-    item.textContent = name;
+    
+    const img = new Image();
+    img.src = source;
+    img.alt = alt;
+
+    item.appendChild(img);
     container.appendChild(item);
 }
 
